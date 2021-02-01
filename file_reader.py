1a726fd0b150e365e448fb228514ba5a5d821265
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-02-01 13:41:01 -0600

    any and all last edits that i made lst week

--- file_reader.py
+++ file_reader.py
@@ -1,27 +1,66 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+# with open('pi_digits.txt') as file_object:
+#     contents = file_object.read()
+#     print(contents.rstrip())
 
 
-filename = 'pi_digits.txt'
+# filename = 'pi_million_digits.txt'
 
-with open(filename) as file_object:
-    for line in file_object:
-        print(line.rstrip())
-        # this prints every line individually and strips 
-        # any blank lines!
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
+# # this method is effective because it allows you to work
+# # with the file outside of the while loop.
 
-with open(filename) as file_object:
+
+# pi_string = ''
+# for line in lines:
+#     pi_string += line.strip()
+# # the rstrip also removed all the spaces!
+# print(pi_string[:52] + "...")
+# print(len(pi_string))
+
+# print("\nCutie birthday function \n")
+
+# birthday = input("Please enter your birthday? (mmddyy) \t")
+# if birthday in pi_string:
+#     print("Congrats your birthday appears in the one million digits of pi!")
+# else:
+#     print("Sorry your birthday does not appear in pi.")
+
+
+# readlines makes it a list to work with throughtout the code
+sec_file_name = 'learned_in_python.txt'
+with open(sec_file_name) as file_object:
     lines = file_object.readlines()
 
-# this method is effective because it allows you to work
-# with the file outside of the while loop.
-for line in lines:
-    print(line.rstrip())
+print(lines)
+
 
 pi_string = ''
 for line in lines:
-    pi_string += line.strip()
-# the rstrip also removed all the spaces!
+    pi_string += line
+
+
+for sent in pi_string:
+    print("The main " + sent)
+
 print(pi_string)
 print(len(pi_string))
+# this one whipped my but thank god for google
+# i would have never though to write this method this way
+# iteriated over every line to find the word!
+for value in lines:
+    x = value.replace('python', 'ruby')
+    print(x)
+        
+
+
+
+
+
+
+
+
+
+
+
+
