55f4e448d798920d7d87329f69585032b97bd4f7
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-02-19 17:54:10 -0600

    the images that have been saved as svg

--- downloading_data/puplation_project.py
+++ downloading_data/puplation_project.py
@@ -1,15 +1,32 @@
+from country_codes import get_country_code
+
 import json
 from pygal.maps.world import COUNTRIES
 from pygal_maps_world import i18n
-from country_codes import get_country_code
 import pygal
+
+
 
 
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
+
+def get_country_code(country_name):
+    """will return the code of any country searched"""
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
+    # if the country was not found don't break code
+    # just continuew looping through and don't print
+    # any error message
+    return None
+
+
+
 #  print only the countrys with info recorded in 2010
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
@@ -17,21 +34,27 @@ for pop_dict in pop_data:
         
         code = get_country_code(country_name)
         if code:
-            print(code + " : " + str(total_population))
-        else:
-            print("Error" + " : " + country_name )
-    # now we can succesfully graph all the int num
+            cc_populations[code] = total_population
+            # the key will be code and the value
+            # will be total poulation.
+       
 
 for country_code in sorted(COUNTRIES.keys()):
     print(country_code, COUNTRIES[country_code])
     # will print the code and name of the country
 
 
-wm = pygal.maps.world.World()
-wm.title = 'North, Central, and South America'
+# wm = pygal.maps.world.World()
+# wm.title = 'North, Central, and South America'
 
-wm.add('North America', {'ca': 34128900, 'us': 678906, 'mx: 678900098'})
+# wm.add('North America', {'ca': 34128900, 'us': 678906, 'mx: 678900098'})
 # wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
 # wm.add('Soth America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf', 'gy', 'pe', 'py', 'sr', 'uy', 've'])
 
-wm.render_to_file('americas.svg')
+# wm.render_to_file('americas.svg')
+
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world populations.svg')
